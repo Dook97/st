128d954a06d48258a92be0418dbdbc419b11a68b
Jan Doskočil <30779172+Dook97@users.noreply.github.com>
2023-06-27 00:00:00 +0000

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:style=Book:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
